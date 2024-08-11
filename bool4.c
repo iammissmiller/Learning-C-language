0e68099c6cbc8a998cf823a4f81b8a5b4e778484
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-12 00:38:50 +0530

    Add files via upload

---
+++ bool4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+
+int a = 23;
+int b = 73;
+
+if (a > b) 
+{
+printf("a is greater\n");
+}
+else
+{
+printf("a is smaller\n");
+}
+printf( %d , "is the result of a > b" , a > b);
+
+
+
+return 0;
+
+}
