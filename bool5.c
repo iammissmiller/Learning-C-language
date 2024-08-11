13961c2ef9f07638269294f7475e204aa2743024
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-12 01:11:42 +0530

    Add files via upload

---
+++ bool5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdboo.h>
+
+int main(void)
+{
+bool a = true;
+int i = 0;
+
+while (a)
+{
+printf("i is %d\n" , i);
+i++;
+}
+
+if(i > 7)
+{
+a = false
+}
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
