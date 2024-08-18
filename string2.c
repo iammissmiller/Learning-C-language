fda46b4a76cd7e066c4dd09b1b93876f37e9fc8a
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-19 00:51:56 +0530

    Add files via upload

---
+++ string2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+
+
+int main(void)
+
+
+{
+    
+
+// Declare string variable
+    
+// as character array
+    
+
+char str[50];
+
+    
+
+printf("Enter the word: ");
+    
+scanf("%s", str);  
+
+// No newline in format string
+    
+
+printf("\nEntered word is: %s\n", str);
+
+
+
+// Clear the input buffer by consuming leftover newline
+    
+getchar();
+
+    
+
+printf("\nEnter the sentence: ");
+    
+scanf("%[^\n]", str);  
+
+// Read until newline
+    
+
+printf("\nEntered sentence is: %s\n", str);
+
+    
+
+return 0;
+
+
+}
