bdf597b8692b87b5727402b3f0bcf25fac3e529b
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-20 21:22:29 +0530

    Add files via upload

---
+++ printvar.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+int num1 = 99;
+int num2 = 2;
+
+printf("the sum of %d and %d is %d\n" , num1 , num2 , num1 + num2);
+
+
+
+return 0;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
