8bb6d5b9b7132878d5b4af536c74b79e508c799c
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-20 23:21:50 +0530

    Add files via upload

---
+++ printprecision.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+int num = 42432;
+float dec = 12.434543;
+char* str = "geeks for geeks";
+
+printf("for intergers : %.10d\n" , num);
+
+printf("for floats : %2f\n" , dec);
+
+printf("for stings : %.5s" , str);
+
+return 0;
+
+}
