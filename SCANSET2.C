16151286753254e9babdc05d3a5455ab1840101c
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-21 23:46:21 +0530

    Add files via upload

---
+++ SCANSET2.C
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+char str[290];
+
+printf("enter a string :");
+
+scanf("%[^l]s" , str);
+
+printf("you entered : %s" , str);
+
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
