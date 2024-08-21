7c42927a7eb3d80706bcf4bca5c4b9fd20eaefe2
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-22 02:04:08 +0530

    Add files via upload

---
+++ getche.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+int main(void)
+
+{
+
+printf("enter any character");
+
+
+getche();
+
+
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
