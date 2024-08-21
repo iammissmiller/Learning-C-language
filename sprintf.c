c6e0d12a115ed2331deba683fa8d2fabb5af6805
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-22 01:34:18 +0530

    Add files via upload

---
+++ sprintf.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+
+char str[345];
+
+int a = 4 , b = 10;
+
+sprintf(str , "%d and %d are even no." , a , b);
+
+printf("%s" , str);
+
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
