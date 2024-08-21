0caffbb016a767babb504a63f9f8df9b671255a6
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-21 23:09:07 +0530

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a, b ;
+
+printf("enter first no :");
+scanf("%d" , &a);
+
+printf("enter second no :");
+scanf("%d" , &b);
+
+printf("A : %d \t B : %d" , a,b);
+
+return 0;
+
+}
