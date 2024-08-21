12a824c88b242aea86b67ee9c51e13b25784ca73
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-21 23:37:57 +0530

    Add files via upload

---
+++ scanset.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+char str[300];
+
+printf("enter the string : ");
+
+scanf("%[A-Z]s" , str);
+
+printf("you entered %s\n" , str);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
