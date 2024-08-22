2e0730c8b9c8b7e8a6ec2d4fb6e21781cf77b0d9
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-23 01:48:38 +0530

    Add files via upload

---
+++ putchar().c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+int main(void)
+
+{
+
+
+char ch;
+
+printf("enter a character :");
+
+ch = getchar();
+
+putchar(ch);
+
+return 0;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
