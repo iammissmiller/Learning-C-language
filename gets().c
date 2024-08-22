875169e071880b164d1a5cee54c54f5a93d1f2a4
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-23 01:54:16 +0530

    Add files via upload

---
+++ gets().c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+
+int main(void)
+
+{
+
+char name[50];
+
+printf("please enter some text :");
+
+gets(name);
+
+printf("you have entered : %s" , name);
+
+return 0;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
