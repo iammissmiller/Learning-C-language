d0c641d7aad2a115bcaa9a40163ec15878bcf8f3
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-23 02:05:59 +0530

    Add files via upload

---
+++ puts().c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+char name[50];
+
+printf("enter your text : ");
+
+gets(name);
+
+printf("your text is :");
+
+puts(name);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
