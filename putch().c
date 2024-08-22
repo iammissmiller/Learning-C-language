b08b6dc341fc17cd3a6ca05185e0728a593798a1
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-23 02:14:16 +0530

    Add files via upload

---
+++ putch().c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+int main(void)
+
+{
+
+char ch;
+
+printf("enter any character : ");
+
+ch = getchar();
+
+printf("\nentered character is : ");
+
+putch(ch);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
