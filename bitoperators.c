5f279bc83034577648d7a0869bb9587772081f3a
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-26 21:24:55 +0530

    Add files via upload

---
+++ bitoperators.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+unsigned int a = 5 , b = 7;
+
+printf("a = %u and b = %u\n" , a , b);
+
+printf("a&b = %u\n" , a & b);
+
+printf("a|b = %u\n" , a | b);
+
+printf("~a = %u\n" , a = ~a);
+
+printf("a^b = %u\n" , a ^ b);
+
+printf("a<<b = %u\n" , a << b);
+
+printf("a>>b = %u\n" , a >> b);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
