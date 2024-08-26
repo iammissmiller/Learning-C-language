c32084c46138a973113248eb4afab468464a91fe
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-26 23:24:32 +0530

    Add files via upload

---
+++ bitoperators4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+
+int x = 14;
+
+printf("x << 1 = %d\n" , x << 1);
+
+printf("x >> 1 = %d\n" , x >> 1);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
