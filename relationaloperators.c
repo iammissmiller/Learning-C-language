f6413a315f72b0b3c6321ffcca80d59cec99ef1b
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-26 21:07:07 +0530

    Add files via upload

---
+++ relationaloperators.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+int a = 10 , b = 9;
+
+if (a > b)
+   printf("a is greater than b\n");
+else
+   printf("a is not greater than b\n");
+
+
+if (a >= b)
+   printf("a is greater than or equal to b\n");
+else
+   printf("a is not greater than or equal to  b\n");
+
+
+if (a < b)
+   printf("a is lesser than b\n");
+else
+   printf("a is not lesser than b\n");
+
+
+if (a <= b)
+   printf("a is lesser than or equal to b\n");
+else
+   printf("a is not lesser than or equal to  b\n");
+
+
+
+if (a == b)
+   printf("a is equal to b\n");
+else
+   printf("a is not equal to b\n");
+
+
+if (a != b)
+   printf("a is not equal to b\n");
+else
+   printf("a is equal to b\n");
+
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
