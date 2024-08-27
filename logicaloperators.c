e150b795478d448f9687bd70f7130752e38782a0
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-27 23:09:07 +0530

    Add files via upload

---
+++ logicaloperators.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+int a = 10 , b = 20;
+
+if (a > 0 && b > 0) {
+
+printf("both values are greater than zero\n");
+
+}
+
+
+else {
+
+printf("Both values are less than 0\n");
+
+}
+
+return 0 ;
+
+}
