fd9c27ddfd48b6eb5f1adc036ccd07a80ad528fb
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-27 23:49:04 +0530

    Add files via upload

---
+++ assignmentoperators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+int a = 10;
+printf("value of a is %d\n" , a);
+
+a += 10;
+printf("value of a is %d\n" , a);
+
+a -= 10;
+printf("value of a is %d\n" , a);
+
+a *= 10;
+printf("value of a is %d\n" , a);
+
+a /= 10;
+printf("value of a is %d\n" , a);
+
+return 0;
+
+}
