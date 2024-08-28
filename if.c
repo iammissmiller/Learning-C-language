7bf46935b1f58e92e44b490113f81149cf0dc3d4
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-29 00:38:07 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+int gfg = 9;
+
+if(gfg > 10) {
+
+printf("%d is greater than 10" , gfg);
+
+}
+
+if (gfg < 10) {
+
+printf("%d is less than 10" , gfg);
+
+}
+return 0;
+ 
+}
