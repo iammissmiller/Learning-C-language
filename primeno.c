c8bfa02bb40b4dedf125638aed18caaa598b92df
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-29 01:07:46 +0530

    Add files via upload

---
+++ primeno.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main()
+
+{
+
+int n = 19;
+int flag = 0;
+
+for (int i = 2 ; i*i <= n; i++) {
+
+if (n % i == 0) {
+
+flag = 1;
+break;
+
+}
+
+}
+
+printf("%d is " , n);
+
+if(flag == 1) {
+
+printf("not ");
+
+}
+
+printf("a prime no.\n");
+
+return 0;
+
+}
