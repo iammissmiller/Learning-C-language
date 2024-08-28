3958524054380639030e854d6b36d13d48d46c97
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-08-29 00:02:29 +0530

    Add files via upload

---
+++ sizeofoperator4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+int arr[] = {1 ,2 ,3 , 4 ,7 ,98 , 0 , 12 ,35 , 99 14};
+
+printf("number of elements : %lu " , sizeof(arr) / size of(arr[0]));
+
+return 0;
+
+}
