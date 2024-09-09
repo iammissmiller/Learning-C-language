d46266c31e1e2d91d5e27dbec169ed5dae73905d
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-09-09 19:41:41 +0530

    Whileloop2.c

---
+++ Whileloop2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int gfg1 = 1;
+    int gfg2 = 1;
+    
+    while (gfg1 < 10) {
+        
+        printf("infinite ");
+        
+    }
+    
+    return 0;
+    
+}
