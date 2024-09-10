d569f0c23c77085d05efd3bf7e36f7a894db4f0f
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-09-10 23:03:41 +0530

    Dowhile.c

---
+++ Dowhile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    
+    int i = 0;
+    do {
+        
+        printf("do it do it");
+        i++;
+        
+    }
+    while (i < 5) ;
+    
+    return 0;
+}
