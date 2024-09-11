ed6f702f351f0715410d21f3519891a70ee6fcfa
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-09-11 23:19:29 +0530

    Whileloopexample.c

---
+++ Whileloopexample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <stdbool.h>
+
+int main()
+
+{
+    bool condition = false;
+    do {
+        
+        printf("this is a loop body");
+        
+    } while (condition );
+    
+    return 0;
+    
+}
