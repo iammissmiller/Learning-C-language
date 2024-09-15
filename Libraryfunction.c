5521bb711181a08c51a8f8e2c4f79ec979c73f4f
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-09-16 00:37:54 +0530

    Libraryfunction.c

---
+++ Libraryfunction.c
@@ -0,0 +1,17 @@
+#include <math.h>
+
+#include <stdio.h>
+
+int main()
+
+{
+    
+    double number;
+    number= 25;
+    
+    double squareroot = sqrt(number);
+    
+    printf("%lf" , squareroot);
+    
+    return 0;
+}
