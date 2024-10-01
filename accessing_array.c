491297fe1b828ae9e6f8d4667cbab2c71f1732ee
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-10-01 23:50:58 +0530

    accessing_array.c

---
+++ accessing_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    //array declaration and initialization
+    int arr[5] = { 15 , 25, 35 ,45 , 55 };
+    
+    //accessing elements at index 2 i.e 3rd elements
+    printf("element of arr[2] : %d\n " ,  arr[2]);
+    
+    //accessing elements at index 4 i.e last element 
+    printf("element of arr[4] : %d\n " ,  arr[4]);
+    
+    //accesing element at index 0 i.e first element
+    printf("element of arr[0] : %d\n " ,  arr[0]);
+
+    return 0;
+}
