8397989e0d2197b823826ae1f43451089a05959a
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-10-07 22:40:54 +0530

    String1.c

---
+++ String1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main()
+{
+    
+    char str[] = "Geeks" ;
+    
+    printf("%s" , str);
+    
+    int length;
+    
+    length = strlen(str);
+    
+    printf("\n\nLength of the string is : %d" , length);
+    
+    return 0;
+    
+}
