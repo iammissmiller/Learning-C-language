8a2692703dbda6c9e2cac2affb0cdeee026495ce
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-01 22:46:35 +0530

    Add files via upload

---
+++ controlflow1.c
@@ -0,0 +1,29 @@
+//whether no. is positive negative 
+//or zero 
+
+#include <stdio.h>
+
+int main()
+{
+
+int a;
+
+printf("enter a integer a : ");
+
+scanf("%d" , &a);
+
+if (a < 0)
+
+printf("a is negative");
+
+else if (a = 0)
+
+printf("a is zero");
+
+else 
+
+printf("a is positive");
+
+return 0;
+
+}
