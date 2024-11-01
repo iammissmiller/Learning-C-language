d5c6302cc8b1794c9ee7fa19995bea841491470a
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-01 22:49:43 +0530

    Add files via upload

---
+++ controlflow2.c
@@ -0,0 +1,24 @@
+//no. is odd or even 
+
+#include <stdio.h>
+
+int main()
+{
+
+int a ;
+
+printf("enter value of a : ");
+
+scanf("%d" , &a);
+
+if (a % 2 == 0)
+
+printf("a is even");
+
+else 
+
+printf("a is odd");
+
+return 0;
+
+}
