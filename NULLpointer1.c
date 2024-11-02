8da297830ec7ac79230bb14b5126a5e629aeb912
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-02 16:56:21 +0530

    Add files via upload

---
+++ NULLpointer1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+int* ptr = NULL;
+
+if(ptr == NULL) 
+
+printf("pointer does not point to anything");
+
+else 
+
+printf("value pointed by pointer is = %d" , ptr);
+
+return 0;
+
+}
