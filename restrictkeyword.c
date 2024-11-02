cab106f6930c49e8bd8ad4ac78a934991d259ed3
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-03 00:03:13 +0530

    Add files via upload

---
+++ restrictkeyword.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void use(int* a, int* b , int* restrict c)
+{
+
+*a += *c;
+
+*b += *c;
+
+}
+
+
+int main(void)
+{
+
+int a = 50, b = 60 , c = 70;
+
+use(&a,&b,&c);
+
+printf("%d %d %d" , a , b , c);
+
+return 0;
+
+}
