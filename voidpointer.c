7a7e9984f091cdf40f39e4104bba6ddddc0f4e8d
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-02 23:25:29 +0530

    Add files via upload

---
+++ voidpointer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+int x = 4;
+
+float y = 5.5;
+
+void* ptr;
+ptr = &x;
+
+printf("integer variable is = %d" , *((int*)ptr));
+
+ptr = &y;
+
+printf("\nFloat variable is = %f" , *((float*)ptr));
+
+return 0;
+
+}
