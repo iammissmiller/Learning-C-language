6c23ee0bc18dc36a4e690bb5c6f6f9ef335b5c9e
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-08 00:57:53 +0530

    Add files via upload

---
+++ extern.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+extern int a;     // int var;  ->  declaration and definition
+                  // extern int var;  -> declaration
+
+int main() 
+{  
+    printf("%d", a);
+ 
+   return 0;
+}
+
+
+
+
+
+
+
+
+
