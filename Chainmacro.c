e93bb6997b2e96e1960ff9855147186d17abe024
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-15 19:33:44 +0530

    Chainmacro.c

---
+++ Chainmacro.c
@@ -0,0 +1,17 @@
+// C program to illustrate macros
+#include <stdio.h>
+ 
+// Macro definition
+#define INSTAGRAM FOLLOWERS
+#define FOLLOWERS 138
+ 
+// Driver Code
+int main()
+{
+    // Print the message
+    printf("Geeks for Geeks have %dK"
+           " followers on Instagram",
+           INSTAGRAM);
+ 
+    return 0;
+}
