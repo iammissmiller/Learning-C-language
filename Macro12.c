6e66b5314208447abd7f36c20549376830679171
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-19 23:10:45 +0530

    Macro12.c

---
+++ Macro12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define LIMIT 100
+int main()
+{
+    printf("%d", LIMIT);
+// removing defined macro LIMIT
+#undef LIMIT
+    // Next line causes error as LIMIT is not defined
+    printf("%d", LIMIT);
+    return 0;
+}
+// This code is contributed by Santanu
