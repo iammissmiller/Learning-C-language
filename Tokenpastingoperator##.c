e1f3767d7efc797d0902a5224e7e18af86e83c09
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-11-20 00:09:01 +0530

    Tokenpastingoperator##.c

---
+++ Tokenpastingoperator##.c
@@ -0,0 +1,13 @@
+// C program to illustrate (##) operator
+#include <stdio.h>
+
+// Macro definition using the Token-pasting operator
+#define concat(a, b) a##b
+int main(void)
+{
+    int xy = 30;
+
+    // Printing the concatenated value of x and y
+    printf("%d", concat(x, y));
+    return 0;
+} 
