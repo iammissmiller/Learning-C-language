7ac12def7c8242e6bb99dcb98ab8ee7be13ff761
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2024-12-12 23:32:23 +0530

    rewind.c

---
+++ rewind.c
@@ -0,0 +1,20 @@
+// C program to illustrate the use of rewind
+#include <stdio.h>
+
+int main()
+{
+    FILE* fptr;
+    fptr = fopen("file.txt", "w+");
+    fprintf(fptr, "Geeks for Geeks\n");
+
+    // using rewind()
+    rewind(fptr);
+
+    // reading from file
+    char buf[50];
+    fscanf(fptr, "%[^\n]s", buf);
+
+    printf("%s", buf);
+
+    return 0;
+}
