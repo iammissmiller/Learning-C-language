4dc5d5d72f8ca345f34cd4ef44059e57ab2ebeff
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-05 23:20:22 +0530

    Add files via upload

---
+++ time1.c
@@ -0,0 +1,15 @@
+
+//Program to print the date and time of the //system.
+
+
+#include <stdio.h>
+#include <time.h>
+int main(void)
+{
+	struct tm* ptr;
+	time_t t;
+	t = time(NULL);
+	ptr = localtime(&t);
+	printf("%s", asctime(ptr));
+	return 0;
+}
